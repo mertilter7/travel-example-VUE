c9d83c29732979f5b6ef5106cdcd9461d77f419f
mertilter7 <57452662+mertilter7@users.noreply.github.com>
2021-03-22 23:16:33 +0300

    last

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import Home from "./components/Home";
-import Travel from "./components/Travel";
+import Gallery from "./components/Gallery";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Travel from "./components/Travel";
 export const routes = [
-    { path : '/', component : Home},
-    { path : '/travel', component : Travel},
-    { path : '/about', component : About},
-    { path : '/contact', component : Contact}
+    { path: '/', component: Home },
+    { path: '/gallery', component: Gallery },
+    { path: '/about', component: About },
+    { path: '/contact', component: Contact },
+    { path: '/travel', component: Travel }
 
-    
+
 ];
